2c9d1cabf6243aa912b1857c410657616005d0a3
Dario <daminga2@utpl.edu.ec>
2023-11-15 16:54:15 -0500

    Listo

---
+++ tarea/src/main/java/com/mycompany/tarea/Ejercicio4.java
@@ -0,0 +1,70 @@
+package com.mycompany.tarea;
+import java.util.Scanner;
+public class Ejercicio4 {
+    public static void main(String[] args) {
+      Scanner teclado = new Scanner(System.in);
+      int opcion;
+        System.out.println("Selecciones un numero del 1 al 12");
+        System.out.println("==============================================================================================================");
+        System.out.println("[1]");
+        System.out.println("[2]");
+        System.out.println("[3]");
+        System.out.println("[4]");
+        System.out.println("[5]");
+        System.out.println("[6]");
+        System.out.println("[7]");
+        System.out.println("[8]");
+        System.out.println("[9]");
+        System.out.println("[10]");
+        System.out.println("[11]");
+        System.out.println("[12]");
+        System.out.println("==============================================================================================================");
+        opcion = teclado.nextInt();
+        switch (opcion){
+            case 1:
+                System.out.println("Enero");
+                break;
+            case 2:
+                System.out.println("Febrero");
+                break;
+            case 3:
+                System.out.println("Marzo");
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Mayo");
+                break;
+            case 6:
+                System.out.println("Junio");
+                break;
+            case 7:
+                System.out.println("Julio");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Septiembre");
+                break;
+            case 10:
+                System.out.println("Octubre");
+                break;
+            case 11:
+                System.out.println("Nomviebre");
+                break;
+            case 12:
+                System.out.println("Diciembre");
+                break;
+            default:{
+                System.out.println("No existe tal opcion");
+            
+        }
+        }
+      
+      
+    }
+    
+    
+    
+}
---
+++ tarea/src/main/java/com/mycompany/tarea/Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
